a6ce44b5c487de5ed37552d6e23b1b2c06b8d127
Bamidele007 <bamideleriliwan7673@gmail.com>
2024-07-19 19:46:55 +0100

    updated app.py and test.py

--- test.py
+++ test.py
@@ -12,4 +12,4 @@ def client():
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello Realzzy World!" in response.data
+    assert b"Hello Realzzy007 World!" in response.data
