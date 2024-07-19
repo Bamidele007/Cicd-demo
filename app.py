e73730236d5d821d2289f87bd50b6574205af168
Rilwan Bamidele <153334318+Bamidele007@users.noreply.github.com>
2024-07-19 20:28:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from urllib.parse import quote as url_quote
 app = Flask(__name__)
 
+# Other routes and logic
 @app.route("/")
 def hello():
    return "Hello Realzzy007 World!"
